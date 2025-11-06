5ab7f73e52f5e8674140f672a1fad5538f9f6031
Drew Immerman <drewimmerman@gmail.com>
2025-11-06 08:18:02 -0500

    Project created using TypeScript Template (lib)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# {{project-name}} - Readme
+# ts-rest - Readme
 
 ## Contributing & Development
 
--- test/spec/index.spec.ts
+++ test/spec/index.spec.ts
@@ -1,7 +1,7 @@
 import 'jasmine';
 import * as index from '../../src';
 
-describe('{{project-name}}', () => {
+describe('ts-rest', () => {
 	it('exports a', () => {
 		expect(index.a).toBeTrue();
 	});
